6dbd3f7dcd6f9cf2e32f25be4ef58ccb1d3ad4c3
Joelton Batista <joelton_bap@hotmail.com>
2025-08-27 09:55:46 -0300

    apresentação de erros no data

--- Projeto/js/form.js
+++ Projeto/js/form.js
@@ -1,34 +1,28 @@
  // Criando uma classe chamada "contato"
 class contato {
-    constructor(nome, sobrenome, email, cpf, telefone, contato) {
+    constructor(nome, email, telefone, tipo, mensagem) {
         this.nome = nome;
-        this.sobrenome = sobrenome;
         this.email = email;
-        this.cpf = cpf;
         this.telefone = telefone;
-        this.contato = contato;
+        this.tipo = tipo;
+        this.mensagem = mensagem;        
     }
 }
 
 // Função que recebe como parâmetro um formulário HTML e implementa um objeto contato à partir da classe anterior mencionada //
 function Post(form) {
 
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
+  let data = new contato(
+    
+            form.elements.namedItem("nome").value,             
             form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
+            form.elements.namedItem("tipo").value,
+            form.elements.namedItem("mensagem").value);
   
 }
 
 // Função para enviar os dados inseridos sobre contato emitindo uma mensagem de retorno//
-function Enviar() {
-
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }
-
-}
+console.log("dados dos formulários:", data);
+alert(`Obrigado, ${data.nome}! Sua mensagem de ${data.tipo} foi enviado com sucesso.`);
+form.reset();
